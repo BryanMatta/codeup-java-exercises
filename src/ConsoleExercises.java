160ce7e9361ddb944d56f2a552452ba85f2e570b
Bryan Matta <bryan.m.matta@gmail.com>
2019-01-07 14:58:33 -0600

    finished console IO exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-
+        double pi = 3.14159;
+        System.out.format("The value of PI is approximately %.2f%n", pi);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a Number: ");
+        int userNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("You entered: " + userNumber);
+        System.out.print("Please enter three words: ");
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+        scanner.nextLine();
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+        System.out.print("Please enter a full sentence: ");
+        String userSentence = scanner.nextLine();
+        System.out.println(userSentence);
     }
+
 }
